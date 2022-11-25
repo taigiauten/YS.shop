199491cd901784dccb866417e766c4a889ab3f6e
taithangYasuo <phaytai010899@gmail.com>
2022-11-25 11:52:16 +0700

    add file detail-cart

---
+++ js/product-detail.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.product-img-item').forEach(e => {
+    e.addEventListener('click', () => {
+        let img = e.querySelector('img').getAttribute('src')
+        document.querySelector('#product-img > img').setAttribute('src', img)
+    })
+})
